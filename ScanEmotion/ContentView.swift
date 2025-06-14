c6d4ac0b71dbd6ce70a939001a48a9097c522edb
Engin Bolat <engi.bolat@gmail.com>
2025-06-14 12:50:42 +0300

    Initial Commit

---
+++ ScanEmotion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScanEmotion
+//
+//  Created by Engin Bolat on 14.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
